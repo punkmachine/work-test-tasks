e1108afd54a04f2b87df2b324776d8a183888955
PunkMachine <alex_rassydihin@mail.ru>
2022-12-12 16:50:55 +0600

    init.

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<main>
+		<div class="order">
+			<h3 class="order__name">Заказ #1202</h3>
+		</div>
+	</main>
+</template>
+
+<script>
+export default {
+	name: 'App',
+}
+</script>
+
+<style lang="scss" scoped>
+</style>
