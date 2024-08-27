952ac10f73317a2656d815219a8a4dc8c233d7b3
Алекс Рассудихин <76869388+punkmachine@users.noreply.github.com>
2024-08-27 08:09:56 +0500

    init

---
+++ webest/src/App.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+</script>
+
+<template>
+  <header>
+    header text
+  </header>
+  <RouterView />
+</template>
+
+<style lang="scss" scoped>
+header {
+  color: blue;
+  font-size: 22px;
+}
+</style>
---
+++ webest/src/views/HomeView.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <main>
+    main page
+  </main>
+</template>
